8f739b95afdca2d4f4a52197e605ae642ecdc517
ShaanSingh <shaans6@nycstudents.net>
2025-09-15 15:26:31 -0400

    Fixed random number gen

--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         Feeder f = new Feeder(500);
-        System.out.println(f.simulateManyDays(3,1));
+        System.out.println(f);
+        System.out.println(f.getCurrentFood());
+        System.out.println(Math.random());
+        System.out.println(Math.random() *10);
+        System.out.println((int) (Math.random() *10));
+        System.out.println((int) (Math.random() *10) +1);
+        System.out.println(Math.random() < 0.05);
+        f.simulateOneDay(12);
+        System.out.println(f.getCurrentFood());
+        f = new Feeder(1000);
+        f.simulateOneDay(22);
+        System.out.println(f.getCurrentFood());
+        f = new Feeder(2400);
+        System.out.println(f.simulateManyDays(10,4));
+        f = new Feeder(250);
+        System.out.println(f.simulateManyDays(10, 5));
+        f = new Feeder(0);
+        System.out.println(f.simulateManyDays(10, 5));
+
     }
 }
