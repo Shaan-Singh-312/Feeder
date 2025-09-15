ba677976fe24d5c280087ceded9955781a3c72f9
ShaanSingh <shaans6@nycstudents.net>
2025-09-15 15:00:31 -0400

    Fixed random number gen

--- Feeder.java
+++ Feeder.java
@@ -4,16 +4,18 @@ public class Feeder {
      * The amount of food, in grams, currently in the bird feeder; initialized
      * in the constructor and always greater than or equal to zero
      */
+
     private int currentFood;
     public Feeder(int food){
         currentFood = food;
     }
+
     /**
      * Simulates one day with numBirds birds or possibly a bear at the bird
      * feeder, as described in part (a) Precondition: numBirds > 0
      */
-    public void simulateOneDay(int numBirds)
-    {
+
+    public void simulateOneDay(int numBirds){
         if (Math.random() < 0.05) currentFood =0;
         else
         {
@@ -23,16 +25,18 @@ public class Feeder {
             if (currentFood < 0) currentFood =0;
         }
     }
+
     public int getCurrentFood(){
         return currentFood;
     }
+
     /**
      * Returns the number of days birds or a bear found food to eat at the
      * feeder in this simulation, as described in part (b) Preconditions:
      * numBirds > 0, numDays > 0
      */
-    public int simulateManyDays(int numBirds, int numDays) 
-    {
+
+    public int simulateManyDays(int numBirds, int numDays) {
         int days = 0;
         while (numDays > 0){
             if (getCurrentFood() ==0) return days;
